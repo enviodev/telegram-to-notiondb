83dd1a09534f2594da1c98fcec899944ba8702d0
keenbeen32 <kenauvith@gmail.com>
2025-08-13 10:05:26 +0200

    feat: boilerplate code and telegram login

--- dist/telegram-client.d.ts
+++
@@ -1,5 +0,0 @@
-import { TelegramClient } from 'telegram';
-export declare function loginToTelegram(): Promise<void>;
-export declare function getClient(): TelegramClient | null;
-export declare function disconnect(): Promise<void>;
-//# sourceMappingURL=telegram-client.d.ts.map
